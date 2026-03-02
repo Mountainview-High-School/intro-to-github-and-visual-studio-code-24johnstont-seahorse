e879a4954225b001a9d8ffc7b00771f82741253a
24johnstont-seahorse <24johnstont@mountainview.school.nz>
2026-03-03 09:35:11 +1300

    Create Questionnaire.py

---
+++ Questionnaire.py
@@ -0,0 +1,23 @@
+import time
+print("Welcome to the questionnaire")
+question=0
+if question ==0:
+  time.sleep(1)
+  answer=input("Please enter your name")
+question=1
+if question ==1:
+  time.sleep(1)
+  answer=input("Please enter your E-mail")
+question=2
+if question == 2:
+ time.sleep(2)
+answer=input("What's your favourite colour?")
+print("Awesome!")
+question=3
+if question ==3:
+   time.sleep(2)
+answer=input("What do you like to eat?")
+print("That's pretty cool")
+
+
+
