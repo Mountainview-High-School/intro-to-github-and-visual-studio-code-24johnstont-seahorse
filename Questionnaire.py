2726bd5e37da8722146ed30deaced37b4049f216
24johnstont-seahorse <24johnstont@mountainview.school.nz>
2026-03-04 11:45:24 +1300

    Questionnaire.py

--- Questionnaire.py
+++ Questionnaire.py
@@ -1,5 +1,6 @@
 import time
 print("Welcome to the questionnaire")
+questions = [0,1,2,3,4,5,6]
 question=0
 if question ==0:
   time.sleep(1)
@@ -14,10 +15,26 @@ if question == 2:
 answer=input("What's your favourite colour?")
 print("Awesome!")
 question=3
-if question ==3:
+if question==3:
    time.sleep(2)
 answer=input("What do you like to eat?")
 print("That's pretty cool")
+question=4
+if question==4:
+  time.sleep(2)
+answer=input("What do you do for fun?")
+print("That's nice")
+question=5
+if question==5:
+  time.sleep(2)
+answer=input("Do you like sports?")
+print("Okay")
+question=6
+if question==6:
+  time.sleep(2)
+answer=input("What's your favorite type of weather")
+print("That's pretty cool")
+  
 
 
 
